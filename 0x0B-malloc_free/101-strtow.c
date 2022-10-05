99daacd568795a28f136218daf4f11a3c5444e73
Olorunfemi Ayobami <olorunfemiayobami26@gmail.com>
2022-10-04 19:40:40 -0700

    task

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -29,7 +29,8 @@ int count_word(char *s)
 	/**
 	 * **strtow - splits a string into words
 	 * @str: string to split
-	 * Return: pointer to an array of strings (Success) or NULL (Error)
+	 *
+	 * Return: pointer of an array of integers
 	 */
 	char **strtow(char *str)
 	{
